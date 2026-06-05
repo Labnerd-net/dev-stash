046e025aac4ab87c7d6a4bbcdae63c72d06f58d8
Brian Ladner <git@theladners.com>
2026-06-04 22:37:27 -0500

    feature: add offset pagination to item type and collection detail pages

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,4 @@ export const MAX_UPLOAD_SIZE = 25 * 1024 * 1024; // 25 MB
 export const MAX_RECENT_ITEMS = 10;
 export const SEARCH_RESULT_LIMIT = 50;
 export const EXPORT_ZIP_ITEM_LIMIT = 500;
+export const PAGE_SIZE = 50;
